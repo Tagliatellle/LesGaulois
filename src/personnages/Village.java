a745e25382117399ce45b4371e5cc88f92815c06
Mathias Jaroszewicz <jrm4519a@ensfsi.onmicrosoft.com>
2024-09-24 19:54:44 +0200

    ex 1_e tp2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,13 +1,33 @@
 package personnages;
 
+import java.security.PublicKey;
+
 public class Village {
 	private String nom;
-
-	public Village(String nom) {
+	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois=0;
+	
+	public Village(String nom, int nbVillageoisMax) {
 		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMax];
 	}
-
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	
 	public String getNom() {
 		return nom;
 	}
+	
+	public void ajouterHabitant(Gaulois gaulois,) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois++;
+	}
+	
+	public Gaulois trouverHabitant(int numVillageois) {
+		return villageois[numVillageois];
+	}
+	
 }
