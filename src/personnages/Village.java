83e58ea814970a1eee49a447fa8cb050e77e3b4a
Mathias Jaroszewicz <jrm4519a@ensfsi.onmicrosoft.com>
2024-10-01 16:28:29 +0200

    TP2 1_h

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,6 +1,8 @@
 package personnages;
 
 import java.security.PublicKey;
+import personnages.Chef;
+import personnages.Gaulois;
 
 public class Village {
 	private String nom;
@@ -21,7 +23,7 @@ public class Village {
 		return nom;
 	}
 	
-	public void ajouterHabitant(Gaulois gaulois,) {
+	public void ajouterHabitant(Gaulois gaulois) {
 		villageois[nbVillageois] = gaulois;
 		nbVillageois++;
 	}
@@ -30,4 +32,34 @@ public class Village {
 		return villageois[numVillageois];
 	}
 	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + chef + "vivent les légendaires gaulois :");
+		int i =0;
+		while (i<nbVillageois) {
+			System.out.println("- "+ trouverHabitant(i).getNom());
+			i++;
+		}
+	}
+
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles",30);
+		
+	
+
+//		Gaulois gaulois = village.trouverHabitant(30); 
+//		on a créé un tableau de 30 éléments donc de 0 à 29 
+//		et on essaie d'accéder à un 31ème éléments "30"
+		
+		Chef abraracourcix = new Chef("Abraracourcix",6,village);
+		village.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		village.ajouterHabitant(asterix);
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		il n'y a qu'un gaulois asterix situé à la première place (indice 0 et pas 1)
+//		System.out.println(gaulois);
+		Gaulois obelix = new Gaulois("Obélix",25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
+	}
+
 }
